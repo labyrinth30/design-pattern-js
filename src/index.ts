03464392b70012ff19f8238a7175e80275919ed6
younha00 <ayeon0@office.skhu.ac.kr>
2025-02-27 01:24:38 +0900

    싱글톤(Singleton) 패턴 정리

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import Grimpan from "./grimpan.js";
+
+// console.log(Grimpan.getInstance() === Grimpan.getInstance()) // true
+
+// 강결합 되어있는 코드
+// function main(){
+//     Grimpan.getInstance().initialize();
+// }
+
+// 약하게 결합된 코드, 매개변수로 뽑는다.
+function main(instance: any){
+    instance.initialize();
+}
+main(Grimpan.getInstance());
+// main(Editor.getInstance());
